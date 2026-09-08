c897287ee981b023d474717875995b509c1e072b
MEHULI KHANRA <mehulikhanra904@gmail.com>
2026-09-08 20:27:14 +0530

    Normalize student routes for ES modules

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,16 +1,12 @@
-const express = require("express");
-const { getStudentProfile, updateStudentProfile, analyzeSkills, getCareerRoadmap } = require("../controllers/studentController");
-const { verifyToken, studentOnly } = require("../middleware/authMiddleware");
+import express from "express";
+import { getStudentProfile, updateStudentProfile, analyzeSkills, getCareerRoadmap } from "../controllers/studentController.js";
+import { verifyToken, studentOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/skill-gap", verifyToken, studentOnly, analyzeSkills);
 router.get("/roadmap", verifyToken, studentOnly, getCareerRoadmap);
-
-// verifyToken attaches the logged-in user to req.user; studentOnly prevents
-// college and company accounts from reading or changing a student profile.
-router
-  .route("/profile")
+router.route("/profile")
   .get(verifyToken, studentOnly, getStudentProfile)
   .put(verifyToken, studentOnly, updateStudentProfile);
 
