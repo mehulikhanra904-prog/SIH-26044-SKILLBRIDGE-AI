85f0b2e4763687b235eba722dc0a065081fe8e32
Saptak Paul <shantupaul2@gmail.com>
2026-09-09 16:02:24 +0530

    Normalize backend routes directory casing

--- backend/Routes/jobRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.status(501).json({ message: "Job routes are not implemented yet" });
-});
-
-export default router;
